2ded13d1f2693bac5723a7f8c518215039847d64
Cassie Cheung <me@soopy.moe>
2023-08-30 16:37:34 +0800

    fix: stuff

--- flake.nix
+++ flake.nix
@@ -5,9 +5,15 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.staticly = nixpkgs.lib.callPackage ./staticly;
-
-    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.hello;
-  };
+  outputs = { self, nixpkgs }: 
+    let
+      pkgs = import nixpkgs { 
+        system = "x86_64-linux";
+      };
+    in {
+      packages.x86_64-linux = {
+        default = nixpkgs.legacyPackages.x86_64-linux.hello;
+        staticly = pkgs.callPackage ./staticly {};
+      };
+    };
 }
