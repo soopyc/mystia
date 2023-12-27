897aa7273dfcc933474ebb9d5184d29f52aae8bf
Cassie Cheung <me@soopy.moe>
2023-12-27 20:13:01 +0800

    refactor: rearrange inputs in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "Just a bunch of packages";
 
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
   nixConfig = {
     extra-substituters = [
       "https://nonbunary.soopy.moe/gensokyo-global"
@@ -11,15 +20,6 @@
       "gensokyo-global:XiCN0D2XeSxF4urFYTprR+1Nr/5hWyydcETwZtPG6Ec="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
-  };
-
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
   outputs = {
