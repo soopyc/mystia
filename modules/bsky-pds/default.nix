98b46f80e29a095d75960e17dae4c64c1a227b89
Cassie Cheung <me@soopy.moe>
2024-10-26 00:12:32 +0800

    nixos/bsky-pds: try another approach

--- modules/bsky-pds/default.nix
+++ modules/bsky-pds/default.nix
@@ -3,13 +3,39 @@
   config,
   pkgs,
   ...
-}: let
+}:
+let
   inherit (lib) mkOption types;
   cfg = config.services.bsky-pds;
-in {
+  systemdHardening = {
+    CapabilityBoundingSet = null;
+    NoNewPrivileges = true;
+    ProtectSystem = "strict";
+    ProtectHome = true;
+    PrivateTmp = true;
+    PrivateDevices = true;
+    PrivateUsers = true;
+    ProtectClock = true;
+    ProtectHostname = true;
+    ProtectKernelLogs = true;
+    ProtectKernelModules = true;
+    ProtectKernelTunables = true;
+    ProtectControlGroups = true;
+    RestrictAddressFamilies = [ "AF_INET" ];
+    RestrictNamespaces = true;
+    LockPersonality = true;
+    MemoryDenyWriteExecute = false;
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+    RemoveIPC = true;
+    PrivateMounts = true;
+    SystemCallArchitectures = "native";
+  };
+in
+{
   options.services.bsky-pds = {
     enable = lib.mkEnableOption "Bluesky Personal Data Server (PDS)";
-    package = lib.mkPackageOption pkgs "bsky-pds" {};
+    package = lib.mkPackageOption pkgs "bsky-pds" { };
 
     initSecrets =
       lib.mkEnableOption {
@@ -17,16 +43,29 @@ in {
           the generation of secrets for every required secret file not found.
           As of now this automcatically generates secrets for the JWT secret, admin password and the PLT rotation key.
 
+          This will not overwrite existing secrets.
+
           If you disable this option, you will have to manually generate the secrets.
           The commands to do so are listed in the PDS installation script
         '';
       }
-      // {default = true;};
+      // {
+        default = true;
+      };
 
     # pds config definition
     settings = mkOption {
       type = types.submodule {
-        freeformType = types.attrsOf (types.oneOf (with types; [bool int str]));
+        freeformType = types.attrsOf (
+          types.oneOf (
+            with types;
+            [
+              bool
+              int
+              str
+            ]
+          )
+        );
         options = {
           PDS_HOSTNAME = mkOption {
             description = "The hostname of the PDS.";
@@ -101,28 +140,31 @@ in {
     };
 
     credentials = mkOption {
-      default = {};
+      default = { };
       type = types.submodule {
         freeformType = types.attrsOf types.str;
-        options = let
-          mkFile = file: description:
-            mkOption {
-              type = types.str;
-              default = "${cfg.settings.PDS_DATA_DIRECTORY}/${file}";
-              defaultText = lib.literalExpression "\${settings.PDS_DATA_DIRECTORY}/${file}";
-              description = "File path for ${description}";
-            };
-        in {
-          PDS_JWT_SECRET = mkFile "jwt_key" "JWT signing secret";
-          PDS_ADMIN_PASSWORD = mkFile "admin_passwd" "PDS administrator password";
-          PDS_PLC_ROTATION_KEY_K256_PRIVATE_KEY_HEX = mkFile "plc_rotation_key" "PLC k256 hex format rotation key";
-        };
+        options =
+          let
+            mkFile =
+              file: description:
+              mkOption {
+                type = types.str;
+                default = "${cfg.settings.PDS_DATA_DIRECTORY}/${file}";
+                defaultText = lib.literalExpression "\${settings.PDS_DATA_DIRECTORY}/${file}";
+                description = "File path for ${description}";
+              };
+          in
+          {
+            PDS_JWT_SECRET = mkFile "jwt_key" "JWT signing secret";
+            PDS_ADMIN_PASSWORD = mkFile "admin_passwd" "PDS administrator password";
+            PDS_PLC_ROTATION_KEY_K256_PRIVATE_KEY_HEX = mkFile "plc_rotation_key" "PLC rotation key";
+          };
       };
       description = ''
         Attribute list of environment variables stored as files outside of the Nix store for security.
 
-        Each attribute should be a string pointing to a file, whose content will be replaced during service
-        startup.
+        Each attribute should be a string pointing to a file, whose content will be read and set as environment variables
+        with the same name during service startup.
 
         If the file does not exist and [`initSecrets`] is enabled, known secret values will have a new secret
         generated and placed in the file specified.
@@ -135,94 +177,89 @@ in {
       bsky-pds = {
         enable = true;
         description = "Bluesky Personal Data Server";
-        documentation = ["https://github.com/bluesky-social/pds"];
+        documentation = [ "https://github.com/bluesky-social/pds" ];
         environment = builtins.mapAttrs (_: builtins.toString) cfg.settings;
         # This is golfed. full form should be builtins.mapAttrs (name: value: builtins.toString value) cfg.settings.
 
-        after = ["network-online.target"] ++ lib.optionals cfg.initSecrets ["bsky-pds-secrets.service"];
-        wants = ["network-online.target"];
-        requires = lib.optionals cfg.initSecrets ["bsky-pds-secrets.service"];
-
-        script = ''
-          ##### Pre-flight check and variable loading phase #####
-          # systemd has checks on activation and if a credential doesn't exist the service just fails before anything is executed.
-          # in this unit we only load the credentials passed by systemd.
-          ${
-            lib.concatLines (lib.mapAttrsToList (name: _: ''
-                export ${name}=$(cat ''${CREDENTIALS_DIRECTORY}/${name})
-              '')
-              cfg.credentials)
-          }
-
-          ##### Launch phase #####
-          ${lib.getExe cfg.package}
-        '';
+        after = [
+          "network-online.target"
+          "bsky-pds-secrets.service"
+        ];
+        wants = [ "network-online.target" ];
+        requires = [ "bsky-pds-secrets.service" ];
 
         serviceConfig = {
           Restart = "on-failure";
           RestartSec = 10;
+          ExecStart = lib.getExe cfg.package;
 
           DynamicUser = true;
           StateDirectory = "bsky-pds";
 
           # credentials
-          LoadCredential = lib.mapAttrsToList (name: value: "${name}:${value}") cfg.credentials;
-
-          # Hardening - not sure how many of these are superfluous.
-          PrivateTmp = true;
-          PrivateUsers = true;
-          NoNewPrivileges = true;
-          RestrictSUIDSGID = true;
-          RestrictNamespaces = true;
-          CapabilityBoundingSet = null;
-          SystemCallArchitectures = "native";
-          SystemCallFilter = [
-            "@system-service"
-            "@pkey"
-            "~@privileged"
-            "~@resources"
+          # LoadCredential = lib.mapAttrsToList (name: value: "${name}:${value}") cfg.credentials;
+          EnvironmentFile = [
+            # should be safe to assume this file will exist during service startup as the secret service is ran.
+            "${cfg.settings.PDS_DATA_DIRECTORY}/.secret.env"
           ];
-          RestrictAddressFamilies = [
-            "AF_INET"
-          ];
-        };
+        } // systemdHardening;
 
-        wantedBy = ["multi-user.target"];
+        wantedBy = [ "multi-user.target" ];
       };
 
-      bsky-pds-secrets = lib.mkIf cfg.initSecrets {
-        script = ''
+      bsky-pds-secrets = {
+        script = lib.concatLines [
           ##### Secret generation phase #####
-          if test ! -e ${cfg.credentials.PDS_JWT_SECRET}; then
-            echo "Generating JWT secret..."
-            ${lib.getExe pkgs.openssl} rand --hex 16 > '${cfg.credentials.PDS_JWT_SECRET}'
-          fi
+          (lib.optionalString cfg.initSecrets ''
+            if test ! -e ${cfg.credentials.PDS_JWT_SECRET}; then
+              echo "Generating JWT secret..."
+              ${lib.getExe pkgs.openssl} rand --hex 16 > '${cfg.credentials.PDS_JWT_SECRET}'
+            fi
 
-          if test ! -e ${cfg.credentials.PDS_ADMIN_PASSWORD}; then
-            echo "Generating Admin Password..."
-            ${lib.getExe pkgs.openssl} rand --hex 16 > '${cfg.credentials.PDS_ADMIN_PASSWORD}'
-          fi
+            if test ! -e ${cfg.credentials.PDS_ADMIN_PASSWORD}; then
+              echo "Generating Admin Password..."
+              ${lib.getExe pkgs.openssl} rand --hex 16 > '${cfg.credentials.PDS_ADMIN_PASSWORD}'
+            fi
 
-          # this command exists as-is in the official installation script, but i'm not sure if this is just a sophisticated way of running
-          # openssl rand -hex 32 and if there are other implications by doing so. i don't know anything about crypto so i will not touch this.
-          if test ! -e ${cfg.credentials.PDS_PLC_ROTATION_KEY_K256_PRIVATE_KEY_HEX}; then
-            echo "Generating PLC rotation key..."
-            ${lib.getExe pkgs.openssl} ecparam --name secp256k1 --genkey --noout --outform DER | \
-              tail --bytes=+8 | \
-              head --bytes=32 | \
-              ${lib.getExe pkgs.unixtools.xxd} --plain --cols 32 > '${cfg.credentials.PDS_PLC_ROTATION_KEY_K256_PRIVATE_KEY_HEX}'
-          fi
-        '';
+            # this command exists as-is in the official installation script, but i'm not sure if this is just a sophisticated way of running
+            # openssl rand -hex 32 and if there are implications of doing so. i don't know anything about crypto so i will not touch this.
+            if test ! -e ${cfg.credentials.PDS_PLC_ROTATION_KEY_K256_PRIVATE_KEY_HEX}; then
+              echo "Generating PLC rotation key..."
+              ${lib.getExe pkgs.openssl} ecparam --name secp256k1 --genkey --noout --outform DER | \
+                tail --bytes=+8 | \
+                head --bytes=32 | \
+                ${lib.getExe pkgs.unixtools.xxd} --plain --cols 32 > '${cfg.credentials.PDS_PLC_ROTATION_KEY_K256_PRIVATE_KEY_HEX}'
+            fi
+          '')
+
+          # should we also check if secrets exist here before setting the .env file?
+
+          ##### Secret loading phase #####
+          ''
+            cat > "${cfg.settings.PDS_DATA_DIRECTORY}/.secret.env" <<EOF
+            ${
+              (lib.concatLines (
+                lib.mapAttrsToList (name: value: ''
+                  ${name}=$(cat ${value})
+                '') cfg.credentials
+              ))
+            }
+            EOF
+          ''
+        ];
 
         serviceConfig = {
           Type = "oneshot";
           RemainAfterExit = true;
-          DynamicUser = true;
-          StateDirectory = "bsky-pds";
-        };
+          UMask = "0077";
+
+          # sandboxing - especially important here as we run as a privileged user to read credentials.
+          PrivateNetwork = true;
+          ReadWritePaths = [cfg.settings.PDS_DATA_DIRECTORY];
+        } // systemdHardening;
       };
     };
   };
 
-  meta.maintainers = with lib.maintainers; [soopyc];
+  meta.maintainers = with lib.maintainers; [ soopyc ];
 }
