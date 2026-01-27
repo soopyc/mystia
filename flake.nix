30057f5a7b33b7275a8f70feae6c7692a212bd49
Sophie Cheung <git@soopy.moe>
2026-01-28 04:30:07 +0800

    flake: replace pkgs.system with actual system

--- flake.nix
+++ flake.nix
@@ -34,38 +34,55 @@
         "aarch64-linux"
         "x86_64-darwin"
       ];
-      forAllSystems = fn: nixpkgs.lib.genAttrs systems (system: fn nixpkgs.legacyPackages.${system});
+      forAllSystems =
+        fn:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          fn {
+            inherit system;
+            pkgs = nixpkgs.legacyPackages.${system};
+          }
+        );
 
-      treefmtEval = forAllSystems (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+      treefmtEval = forAllSystems ({ pkgs, ... }: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
     in
     {
-      packages = forAllSystems (pkgs: import ./packages/all-packages.nix inputs { } pkgs);
+      packages = forAllSystems ({ pkgs, ... }: import ./packages/all-packages.nix inputs { } pkgs);
       overlays.default = import ./packages/all-packages.nix inputs;
 
-      formatter = forAllSystems (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
-      checks = forAllSystems (pkgs: {
-        formatting = treefmtEval.${pkgs.system}.config.build.check self;
-      });
+      formatter = forAllSystems ({ system, ... }: treefmtEval.${system}.config.build.wrapper);
+      checks = forAllSystems (
+        { system, ... }:
+        {
+          formatting = treefmtEval.${system}.config.build.check self;
+        }
+      );
 
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShellNoCC {
-          packages = [ nix-update-soopy.packages.${pkgs.system}.default ];
-        };
-      });
+      devShells = forAllSystems (
+        { pkgs, system }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = [ nix-update-soopy.packages.${system}.default ];
+          };
+        }
+      );
 
       nixosModules = {
         fixups = lib.modules.importApply ./modules/fixups { };
         vmauth = lib.modules.importApply ./modules/vmauth { };
         arrpc = lib.modules.importApply ./modules/arrpc { };
         bsky-pds = lib.modules.importApply ./modules/bsky-pds { };
-        mautrix-discord = lib.modules.importApply ./modules/mautrix-discord.nix {};
+        mautrix-discord = lib.modules.importApply ./modules/mautrix-discord.nix { };
         anubis = lib.modules.importApply ./modules/anubis { inherit self; };
       };
 
-      nixosTests = forAllSystems (pkgs: {
-        anubis = pkgs.callPackage ./tests/anubis.nix { } {
-          module = self.nixosModules.anubis;
-        };
-      });
+      nixosTests = forAllSystems (
+        { pkgs, ... }:
+        {
+          anubis = pkgs.callPackage ./tests/anubis.nix { } {
+            module = self.nixosModules.anubis;
+          };
+        }
+      );
     };
 }
